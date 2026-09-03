bd934599312524e73c316970449c4e10fab6ad55
Teemo1423 <ixx321@naver.com>
2026-09-03 23:34:17 +0900

    Load ministry groups editor in admin

--- js/admin-content-pro.js
+++ js/admin-content-pro.js
@@ -1,1 +1,1 @@
-(()=>{const load=src=>{const s=document.createElement('script');s.src=src;document.head.appendChild(s)};load('/js/admin-content-pro2.js?v=contentpro2');load('/js/admin-weekly.js?v=weekly1')})();
+(()=>{const load=src=>{const s=document.createElement('script');s.src=src;document.head.appendChild(s)};load('/js/admin-content-pro2.js?v=contentpro2');load('/js/admin-weekly.js?v=weekly1');load('/js/admin-ministry-groups.js?v=ministrygroups1')})();
